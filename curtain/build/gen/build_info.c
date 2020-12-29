3f672671eb4313c35bb9ca3c63d10b9220be09ec
sumancvb <sumancvb@gmail.com>
2020-12-29 15:23:06 +0530

    build

--- curtain/build/gen/build_info.c
+++ curtain/build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20201229-093920";
-const char *build_timestamp = "2020-12-29T09:39:20Z";
+const char *build_id = "20201229-095214";
+const char *build_timestamp = "2020-12-29T09:52:14Z";
 const char *build_version = "1.0.0";
--- curtain/build/gen/mg_build_info.c
+++ curtain/build/gen/mg_build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *mg_build_id = "20201229-093920/2.18.0-g24c520d";
-const char *mg_build_timestamp = "2020-12-29T09:39:20Z";
+const char *mg_build_id = "20201229-095214/2.18.0-g24c520d";
+const char *mg_build_timestamp = "2020-12-29T09:52:14Z";
 const char *mg_build_version = "2.18.0";
