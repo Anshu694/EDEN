a4e97f8c67b75830edb12a0c0e66d104f189add6
sumancvb <sumancvb@gmail.com>
2020-12-29 15:18:02 +0530

    build

--- curtain/build/gen/build_info.c
+++ curtain/build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20201229-092801";
-const char *build_timestamp = "2020-12-29T09:28:01Z";
+const char *build_id = "20201229-093920";
+const char *build_timestamp = "2020-12-29T09:39:20Z";
 const char *build_version = "1.0.0";
