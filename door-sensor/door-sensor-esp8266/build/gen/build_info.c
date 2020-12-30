8a29a6c2b98c7717cf230156f973a581ecf2313a
sumancvb <sumancvb@gmail.com>
2020-12-30 14:40:58 +0530

    update

--- door-sensor/door-sensor-esp8266/build/gen/build_info.c
+++ door-sensor/door-sensor-esp8266/build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20201230-084342";
-const char *build_timestamp = "2020-12-30T08:43:42Z";
+const char *build_id = "20201230-090225";
+const char *build_timestamp = "2020-12-30T09:02:25Z";
 const char *build_version = "1.0.1";
