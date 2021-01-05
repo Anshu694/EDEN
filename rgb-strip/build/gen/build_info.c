3b905baa0ed0f7616c9db3dfd6329bfc5c2dd1a7
sumancvb <sumancvb@gmail.com>
2021-01-05 19:22:52 +0530

    rgb PWM freq set to 250

--- rgb-strip/build/gen/build_info.c
+++ rgb-strip/build/gen/build_info.c
@@ -1,4 +1,4 @@
 /* Auto-generated, do not edit. */
-const char *build_id = "20201231-073537";
-const char *build_timestamp = "2020-12-31T07:35:37Z";
+const char *build_id = "20210105-135124";
+const char *build_timestamp = "2021-01-05T13:51:24Z";
 const char *build_version = "1.0.2";
